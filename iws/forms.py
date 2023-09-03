ccc931f96d0668059e6315db472d76b22fa0924a
manish1026 <manish.sharma291086@gmail.com>
2023-09-03 01:03:30 -0600

    Code  commit

---
+++ iws/forms.py
@@ -0,0 +1,13 @@
+# from django import forms
+
+
+# class CreateNewForm(forms.Form):
+#    username= forms.CharField(label='Username', required="True" )
+#    firstname= forms.CharField(label='First Name', required="True")
+#    lastname= forms.CharField(label="Last Name", required="False" )
+#    email= forms.EmailField(label='Email ID', required="True")
+#    password=forms.PasswordInput('Password')
+#    confirmpassword=forms.PasswordInput()
+   
+
+# # class Calculator(forms.Form):
